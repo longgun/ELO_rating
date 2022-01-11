f1a3778da3fdaa6eaae646024c9b90c321b0ec30
longgun <eddie.won.924@gmail.com>
2022-01-11 16:45:22 +0900

    starting test code

--- elo_rating.py
+++ elo_rating.py
@@ -1,11 +1,144 @@
-def calc_rating(elo_p1: int, records: list, k=16):
-    encode = {"w": 1, "l": 0}
+import csv
+import datetime
+import pandas as pd
 
-    for record, elo_oppo in records:
-        expected_wins = 1 / (1 + 10 ** ((elo_oppo - elo_p1) / 400))
 
-        print(elo_oppo, expected_wins)
-        elo_p1 = elo_p1 + k * (encode[record] - expected_wins)
+class Teams:
+    def __init__(self):
+        self.teams_elo = dict()
+        self.teams_games = dict()
 
-    return elo_p1
+    def get_teams_elo(self):
+        return self.teams_elo
 
+    def get_teams_games(self):
+        return self.teams_games
+
+    def init_teams(self, teams: list):
+        for team in teams:
+            if team not in self.teams_elo:
+                self.teams_elo[team] = 1500
+                self.teams_games[team] = 0
+
+    def adjust(self, name, score):
+        self.teams_elo[name] = score
+        self.teams_games[name] += 1
+
+    def get_score(self, name):
+        return self.teams_elo[name]
+
+    def get_games(self, name):
+        return self.teams_games[name]
+
+
+class ELOMaker:
+    def __init__(self) -> None:
+        self.teams = Teams()
+
+    def read_nba_data(self, path: str) -> None:
+        names = {"january": 13, "november": 11, "october": 10, "december": 12}
+
+        for csv_file, number in sorted(names.items(), key=(lambda x: x[1])):
+            csv_path = f"{path}/{csv_file}.csv"
+            csv_df = self.read_file(csv_path)
+            teams = list(csv_df["wins"].unique())
+            teams += list(csv_df["loses"].unique())
+            teams = set(teams)
+            self.teams.init_teams(teams)
+            csv_df = self.assign_rating(csv_df)
+
+        sorted_teams = sorted(self.teams.get_teams_elo().items(), key=(lambda x: x[1]))
+        for team, score in sorted_teams:
+            print(team, score, self.teams.get_games(team))
+        print(len(sorted_teams))
+
+    def assign_rating(self, dataframe: pd.DataFrame):
+        winner_rating = list()
+        loser_rating = list()
+        for _, row in dataframe.iterrows():
+            winner = row["wins"]
+            loser = row["loses"]
+
+            winner_elo, loser_elo = self.adjusted_rating(winner, loser)
+            winner_rating.append(winner_elo)
+            loser_rating.append(loser_elo)
+            self.teams.adjust(winner, winner_elo)
+            self.teams.adjust(loser, loser_elo)
+
+        dataframe["winnerRating"] = winner_rating
+        dataframe["loserRating"] = loser_rating
+
+        return dataframe
+
+    def adjusted_rating(self, winner, loser, k=16):
+        elo_win, elo_lose = self.teams.get_score(winner), self.teams.get_score(loser)
+        expected_wins = 1 / (1 + 10 ** ((elo_win - elo_lose) / 400))
+        expected_lose = 1 / (1 + 10 ** ((elo_lose - elo_win) / 400))
+        elo_win = elo_win + k * (1 - expected_wins)
+        elo_lose = elo_lose + k * (0 - expected_lose)
+
+        return elo_win, elo_lose
+
+    def adjusted_rating_double(self, winners, losers, k=16):
+        elo_wins, elo_loses = 1, 2
+        return elo_wins, elo_loses
+
+    def read_file(self, csv_path: str):
+        cal = {"Dec": 12, "Nov": 11, "Oct": 10, "Jan": 1}
+        res_df = {
+            "dates": list(),
+            "wins": list(),
+            "loses": list(),
+            "winsPoints": list(),
+            "losesPoints": list(),
+        }
+        with open(csv_path) as filepath:
+            next(filepath)
+            data = csv.DictReader(
+                filepath,
+                fieldnames=[
+                    "Date",
+                    "start",
+                    "visitor",
+                    "visitorPoints",
+                    "home",
+                    "homePoints",
+                ],
+            )
+            for row in data:
+                winner, loser, w_points, l_points = self.winorlose(row)
+                _, month, day, year = row["Date"].split()
+                date = datetime.date(int(year), cal[month], int(day))
+                res_df["dates"].append(date)
+                res_df["wins"].append(winner)
+                res_df["loses"].append(loser)
+                res_df["winsPoints"].append(w_points)
+                res_df["losesPoints"].append(l_points)
+
+        res_df = pd.DataFrame(res_df)
+
+        return res_df
+
+    def winorlose(self, row: dict):
+        if int(row["visitorPoints"]) > int(row["homePoints"]):
+            return (
+                row["visitor"],
+                row["home"],
+                row.get("visitorPoints", 0),
+                row.get("homePoints", 0),
+            )
+        else:
+            return (
+                row["home"],
+                row["visitor"],
+                row.get("homePoints", 0),
+                row.get("visitorPoints", 0),
+            )
+
+
+if __name__ == "__main__":
+    import sys
+
+    OBJ = ELOMaker()
+
+    OBJ.read_nba_data(sys.argv[1])
--- test_elo_rating.py
+++ test_elo_rating.py
@@ -1,16 +1,31 @@
+from _pytest.fixtures import yield_fixture
 import pytest
-from elo_rating import calc_rating
+from elo_rating import Teams
 
 
-def test_elo_rating():
-    observation = [("w", 1500), ("w", 1500), ("l", 1500), ("w", 1500)]  ## 4연승
-    A_rating = 1500
-    # B_rating = 1500
-
-    adjusted_A_rating = calc_rating(A_rating, observation)
-
-    print(adjusted_A_rating)
+@pytest.fixture(scope="class")
+def team_obj():
+    res = Teams()
+    yield res
+    del res
 
 
-def test_get_history():
-    pass
+def elo_obj():
+    res = Teams()
+    yield res
+    del res
+
+
+def test_init_teams(team_obj):
+    obs = ["a", "b", "c"]
+    pred_elo = {"a": 1500, "b": 1500, "c": 1500}
+    pred_games = {"a": 0, "b": 0, "c": 0}
+
+    team_obj.init_teams(obs)
+
+    assert team_obj.teams_elo == pred_elo
+    assert team_obj.teams_games == pred_games
+
+
+def test_init_teams(team_obj):
+    obs = ["a", "b", "c"]
