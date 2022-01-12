b9a0d86f44d610267385d73cdb936246c34acb77
longgun <eddie.won.924@gmail.com>
2022-01-12 15:49:07 +0900

    flask png out

--- flask_test.py
+++ flask_test.py
@@ -1,17 +1,43 @@
-from flask import Flask
-from nba_elo import nba_elo_parser
+import random
+from io import BytesIO
+
+import matplotlib
+import matplotlib.pyplot as plt
+import numpy as np
 import pandas as pd
+from flask import Flask, send_file
+
+from nba_elo import nba_elo_parser
+
+matplotlib.use("Agg")
 
 app = Flask(__name__)
+
+df = nba_elo_parser("./nba_schedule_and_results")
+max_length = 0
+for team, scores in df.items():
+    if max_length < len(scores):
+        max_length = len(scores)
+
+
+@app.route("/stats")
+def stats():
+    for scores in df.values():
+        while len(scores) < max_length:
+            scores.append(np.nan)
+    return pd.DataFrame(df).to_html()
 
 
 @app.route("/")
 def home():
-    df = nba_elo_parser("./nba_schedule_and_results")
+    plt.figure(figsize=(15, 15))
     for team, scores in df.items():
-        while len(scores) < 50:
-            scores.append(scores[-1])
-    return pd.DataFrame(df).to_html()
+        plt.plot(scores)
+        plt.text(len(scores) - 1, scores[-1] + random.uniform(-1, 1) * 10, team)
+    img = BytesIO()
+    plt.savefig(img, format="png", dpi=200)
+    img.seek(0)
+    return send_file(img, mimetype="image/png")
 
 
 if __name__ == "__main__":
